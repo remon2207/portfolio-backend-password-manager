f854227a89c873217d9b87b21e11af027221d4fb
remon2207.develop@gmail.com <remon2207>
2023-10-08 13:24:13 +0900

    feat: 単一ユーザーを取得する

--- src/users.ts
+++ src/users.ts
@@ -8,3 +8,23 @@ router.route('/').get(async (_, res) => {
 
   res.json(allUsers)
 })
+
+router.route('/:id').get(async (req, res) => {
+  const id = Math.trunc(Number(req.params.id))
+
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    res.json(user)
+  } catch {
+    const error = {
+      title: 'ユーザーが見つかりませんでした',
+      code: 500,
+    }
+    res.status(500).json(error)
+  }
+})
