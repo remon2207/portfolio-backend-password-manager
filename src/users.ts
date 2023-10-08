39ca9afd3d5a21839761472ca6e5416d9bb34682
remon2207.develop@gmail.com <remon2207>
2023-10-08 13:23:46 +0900

    feat: 全ユーザーを取得する

--- src/users.ts
+++ src/users.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { prisma } from './utils/prisma'
+import { prisma } from '@/utils/prisma'
 
 export const router = express.Router()
 
-router.route('/').get(async (req, res) => {
+router.route('/').get(async (_, res) => {
   const allUsers = await prisma.user.findMany()
 
-  res.json({ allUsers })
+  res.json(allUsers)
 })
